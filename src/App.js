2975253936d62c3f28904619eff456c676e8eed3
amirMohammad <amyrmhmdahmdy179@gmail.com>
2025-02-20 01:24:35 -0800

    header created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="parking-container m-0 p-0 w-full min-h-screen flex-col justify-center bg-[url('./images/bg.jpg')] bg-top bg-[length:170%]">
+      <header className="w-full h-[4rem] bg-[gray] flex justify-center items-center">
+        <h1 className="font-bold text-[30px] capitalize">resalat parking</h1>
+      </header>
+
+    </div>
+  );
 }
 
 export default App;
